93886e32955adb8abd65d88ee391818a1e961b9c
Vitali Novogran <vi19954@gmail.com>
2022-06-06 19:13:06 +0300

    Add Private policy text with two clickable spans

--- app/src/main/java/com/example/androidexperiments/MainActivity.kt
+++ app/src/main/java/com/example/androidexperiments/MainActivity.kt
@@ -1,22 +1,56 @@
 package com.example.androidexperiments
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.main_activity_text)
-        val newTextViewText = "Code edited text"
+        val agreementTextView = findViewById<TextView>(R.id.agreementTextView)
+        val fullText = getString(R.string.agreement_full_text)
+        val confidential = getString(R.string.confidential_info)
+        val policy = getString(R.string.privacy_policy)
+        val spannableString = SpannableString(fullText)
 
-        textView.text = newTextViewText
+        val confidentialClickable = MyClickableSpan {
+            Snackbar.make(it,"Go to link1", Snackbar.LENGTH_LONG).show()
+        }
+
+        val policyClickable = MyClickableSpan {
+            Snackbar.make(it,"Go to link2", Snackbar.LENGTH_LONG).show()
+        }
+
+        spannableString.setSpan(
+            confidentialClickable,
+            fullText.indexOf(confidential),
+            fullText.indexOf(confidential)+confidential.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannableString.setSpan(
+            policyClickable,
+            fullText.indexOf(policy),
+            fullText.indexOf(policy)+policy.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        agreementTextView.run {
+            text = spannableString
+            movementMethod = LinkMovementMethod.getInstance()
+            highlightColor = Color.TRANSPARENT
+        }
+
     }
-
-
 }
