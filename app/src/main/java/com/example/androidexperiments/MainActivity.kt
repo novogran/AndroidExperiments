8cde6a625485932ad671ae29dd8a5aa2c93b2f6a
Vitali Novogran <vi19954@gmail.com>
2022-06-06 17:30:47 +0300

    Create Project

---
+++ app/src/main/java/com/example/androidexperiments/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.androidexperiments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.main_activity_text)
+        val newTextViewText = "Code edited text"
+
+        textView.text = newTextViewText
+    }
+
+
+}
